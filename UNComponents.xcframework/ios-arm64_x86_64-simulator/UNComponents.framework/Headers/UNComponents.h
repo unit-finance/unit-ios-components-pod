2f8f4e140c95f3dd60e47dd38249dbccdf0a80e8
Alon Shprung <alonshp1@gmail.com>
2023-04-11 17:02:02 +0300

    add xcframework

---
+++ UNComponents.xcframework/ios-arm64_x86_64-simulator/UNComponents.framework/Headers/UNComponents.h
@@ -0,0 +1,18 @@
+//
+//  UNComponents.h
+//  UNComponents
+//
+//  Created by Alon Shprung on 31/08/2022.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for UNComponents.
+FOUNDATION_EXPORT double UNComponentsVersionNumber;
+
+//! Project version string for UNComponents.
+FOUNDATION_EXPORT const unsigned char UNComponentsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <UNComponents/PublicHeader.h>
+
+
