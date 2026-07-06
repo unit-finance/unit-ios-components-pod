c2e55e4e36162bda16ed3a5fede329bd73d569ec
elaz <elazar@applift-consulting.com>
2026-07-06 14:57:13 +0200

    feat: raise minimum iOS deployment target to 16.4 (UD-16851)

--- Package.swift
+++ Package.swift
@@ -4,7 +4,7 @@ import PackageDescription
 let package = Package(
     name: "UnitComponents",
     platforms: [
-        .iOS(.v14)
+        .iOS("16.4")
     ],
     products: [
         .library(name: "UnitComponents", targets: ["UNComponentsSDKWrapperTarget"])
